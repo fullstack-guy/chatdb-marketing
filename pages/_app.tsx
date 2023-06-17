71efef63482795bfb54174b3e5c8a1c5aae73798
fullstack-guy <andriihryhoriev1@gmail.com>
2023-06-16 21:43:24 -0500

    add stripe with clerk integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { SubscriptionProvider } from "use-stripe-subscription";
 import { hotjar } from "react-hotjar";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
@@ -16,8 +17,16 @@ export default function MyApp({ Component, pageProps }) {
     hotjar.initialize(3430108, 6);
   }, [router.events]);
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <SubscriptionProvider
+      stripePublishableKey={
+        process.env.NODE_ENV === 'production'
+          ? process.env.STRIPE_PROD_PUBLISHABLE_KEY
+          : process.env.STRIPE_TEST_PUBLISHABLE_KEY
+      }
+    >
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </SubscriptionProvider>
   );
 }
