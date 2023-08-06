bc1eecb3bc6edcbb9e4b0b1349a8a1df88ec6855
fullstack-guy <andriihryhoriev1@gmail.com>
2023-08-05 23:07:08 -0500

    revert next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // Prefer loading of ES Modules over CommonJS
-  experimental: { esmExternals: true },
+  reactStrictMode: true,
+  output: "standalone",
 };
