e38565af2016ffa73b4a31a6c8ec63a0d6a2574f
fullstack-guy <andriihryhoriev1@gmail.com>
2023-08-15 09:55:49 -0400

    add margin to blog title

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -30,7 +30,7 @@ export default function Page({ posts }) {
       description="Stay up-to-date with the latest news, insights, and tips about databases, AI technology, and ChatDB features from our blog."
       url="https://www.chatdb.ai/blog"
     >
-      <div className="mt-24 text-center text-7xl font-bold text-black">
+      <div className="mt-24 mb-24 text-center text-7xl font-bold text-black">
         Blog
       </div>
       <div className="m-10 p-4 md:p-0">
