7a9a76fc8cb5a0654dd019f74aba23fea4d16144
fullstack-guy <andriihryhoriev1@gmail.com>
2023-08-30 16:38:15 +0100

    Tweaked checkout page

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -6,10 +6,12 @@ import { set } from "zod";
 
 const Checkout = () => {
     const { isLoaded, isSignedIn, user } = useUser();
+    const router = useRouter()
+    const plan = router.query.plan;
     const [formData, setFormData] = useState({
         email: "",
         country: "",
-        zip: null
+        zip: ""
     })
     const [submitting, setSetsubmitting] = useState(false)
     const plans = {
@@ -20,12 +22,10 @@ const Checkout = () => {
             monthlyPriceId: 'pri_01h90zt3jwcrxsjsmfyzb8qqda'
         }
     }
-
-    const router = useRouter()
+    const capitalizeFirstLetter = (input: string) => input.charAt(0).toUpperCase() + input.slice(1);
     const handlePayment = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const Paddle = window.Paddle;
-        const plan = router.query.plan;
         console.log("plan", plan);
         setSetsubmitting(true)
         Paddle.Checkout.open({
@@ -36,6 +36,11 @@ const Checkout = () => {
                 frameTarget: "checkout-container",
                 frameInitialHeight: "450",
                 frameStyle: "width: 100%; min-width: 312px; background-color: transparent; border: none;"
+            },
+            eventCallback: function (data) {
+                if (data.name == "checkout.completed") {
+                    console.log("paddle data", data);
+                }
             },
             items: [
                 {
@@ -53,9 +58,6 @@ const Checkout = () => {
         });
     };
 
-    useEffect(() => {
-
-    }, []);
     return (
         <Layout
             title="ChatDB Pricing | The AI Database Assistant for your team"
@@ -63,7 +65,9 @@ const Checkout = () => {
             url="https://www.chatdb.ai/pricing"
         >
 
-            <div className="flex items-center justify-center align-middle">
+            <div className="flex flex-col items-center justify-center align-middle">
+                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">You are subscribing for the <span className="capitalize text-blue-600 dark:text-blue-500">{plan}</span> Plan.</h1>
+
                 {
                     !submitting && (
                         <form className="p-4 w-full md:w-1/2 lg:w-1/2 z-[100]" onSubmit={handlePayment}>
@@ -77,7 +81,7 @@ const Checkout = () => {
 
                             <div className="mb-6">
                                 <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Zip or Postal code: <span className="text-sm text-gray-500">{"(Optional)"}</span></label>
-                                <input type="text" id="zip-code" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                                <input type="text" id="zip" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                                     value={formData.zip}
                                     onChange={(e) => setFormData({ ...formData, zip: e.target.value })}
                                 />
