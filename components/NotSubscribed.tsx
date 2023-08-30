f4540c6103443b7aa10948f1e0347f837f94400e
fullstack-guy <andriihryhoriev1@gmail.com>
2023-08-30 09:23:38 +0100

    Fixed not subbed component

--- components/NotSubscribed.tsx
+++ components/NotSubscribed.tsx
@@ -10,7 +10,7 @@ export default function NotSubscribed() {
             </p>
             <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Subscribe to a plan
-                <svg className="w-3.5 h-3.5 ml-2" ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                 </svg>
             </a>
