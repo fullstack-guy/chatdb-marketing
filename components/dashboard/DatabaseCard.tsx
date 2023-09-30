da50f8c92c1c23902a184bc63de4bccf4e0dbf45
fullstack-guy <andriihryhoriev1@gmail.com>
2023-09-30 18:23:37 +0100

    Updated db card component

--- components/dashboard/DatabaseCard.tsx
+++ components/dashboard/DatabaseCard.tsx
@@ -4,7 +4,7 @@ import { trpc } from "../../utils/trpc";
 import UpdateSubscriptionModal from "../UpdateSubscriptionModal";
 import { useRouter } from "next/router";
 import DeleteDatabasesModal from "./DeleteDatabasesModal";
-
+import DropDownMenu from "./DropDownMenu";
 export const Card = ({ logo, title, lastUpdated, uuid }) => {
   const {
     isLoading,
@@ -38,25 +38,30 @@ export const Card = ({ logo, title, lastUpdated, uuid }) => {
 
   return (
     <div className="relative">
-      <div className="cursor-pointer" onClick={handleDatabaseCardClick}>
-        <div className="mb-4 flex cursor-pointer items-center rounded-lg p-4 shadow-md transition duration-300 ease-in-out hover:scale-105">
-          <div className="mr-4 flex h-20 w-20 items-center justify-center overflow-hidden rounded-lg bg-[#0fe0b6]">
-            <Image
-              className="m-auto"
-              width={40}
-              height={40}
-              src={logo}
-              alt={title}
-            />
-          </div>
-          <div>
-            <h1 className="mb-2 text-lg font-bold text-black">{title}</h1>
-            {/* <p className="text-sm italic text-gray-600">
-              Created: {lastUpdated}
-            </p> */}
+
+      <div className="h-[20vh] flex items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 hover:scale-105 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div className="h-full w-1/6 md:h-[20vh] md:w-2/6 lg:h-[20vh] lg:w-2/6 flex items-center justify-center rounded-lg bg-[#0fe0b6]">
+          <Image
+            className="m-auto"
+            width={40}
+            height={40}
+            src={logo}
+            alt={title}
+          />
+        </div>
+        <div className="w-full h-full flex flex-row justify-between items-center p-4 leading-normal">
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+          <div className="h-full flex flex-col justify-between items-center">
+            <DropDownMenu />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green" className="w-6 h-6 cursor-pointer bg-slate-100  hover:bg-slate-200 rounded-xl">
+              <path fillRule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clipRule="evenodd"
+                onClick={handleDatabaseCardClick}
+              />
+            </svg>
           </div>
         </div>
-      </div>
+      </div >
+
       <UpdateSubscriptionModal
         open={isUpdateSubscriptionModalOpeneded}
         setOpen={setIsUpdateSubscriptionModalOpened}
@@ -71,7 +76,7 @@ export const Card = ({ logo, title, lastUpdated, uuid }) => {
         open={isDeleteDatabasesModalOpened}
         setOpen={setIsDeleteDatabasesModalOpeneded}
       />
-    </div>
+    </div >
   );
 };
 
---
+++ components/dashboard/DropDownMenu.tsx
@@ -0,0 +1,64 @@
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+    return (
+        <Menu as="div" className="self-start inline-block text-left">
+            <div>
+                <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-2 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                    </svg>                </Menu.Button>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+
+                        <Menu.Item>
+                            {({ active }) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-red-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    <div className="flex flex-row justify-between items-center">
+                                        Delete
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-6 w-6 cursor-pointer"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="#d72f0c"
+                                            strokeWidth={2}
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                            />
+                                        </svg>
+                                    </div>
+                                </a>
+                            )}
+                        </Menu.Item>
+                    </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
+    )
+}
