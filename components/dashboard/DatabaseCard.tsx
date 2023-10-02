b24c56db91bb8278700e63c6726992b8d4aa613a
fullstack-guy <andriihryhoriev1@gmail.com>
2023-10-02 10:22:20 +0100

    Edited db card dimension

--- components/dashboard/DatabaseCard.tsx
+++ components/dashboard/DatabaseCard.tsx
@@ -38,22 +38,23 @@ export const Card = ({ logo, title, lastUpdated, uuid, refetch }) => {
 
   return (
     <div className="relative">
-
-      <div className="h-[15vh] flex items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 hover:scale-105 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <div className="h-full w-1/6 md:h-[15vh] md:w-2/6 lg:h-[15vh] lg:w-2/6 flex items-center justify-center rounded-lg bg-[#0fe0b6]">
-          <Image
-            className="m-auto"
-            width={40}
-            height={40}
-            src={logo}
-            alt={title}
-          />
+      <div className="h-[15vh] flex justify-center items-center bg-white border border-gray-200 rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 transition duration-300 ease-in-out hover:scale-105 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div className="w-full p-2 md:w-full md:p-4 lg:w-full lg:p-4">
+          <div className="h-[10vh] w-1/3 md:h-[10vh] md:w-2/6 lg:h-[10vh] lg:w-2/6 flex items-center justify-center rounded-lg bg-[#0fe0b6]">
+            <Image
+              className="m-auto p-1"
+              width={40}
+              height={40}
+              src={logo}
+              alt={title}
+            />
+          </div>
         </div>
         <div className="w-full h-full flex flex-row justify-between items-center p-2 leading-normal">
           <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
           <div className="h-full flex flex-col justify-around items-center">
             <DropDownMenu uuid={uuid} refetchDatabases={refetch} />
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green" className="w-8 h-8 cursor-pointer bg-slate-100  hover:bg-slate-200 rounded-xl"
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green" className="w-6 h-6 md:w-8 md:h-8 lg:w-8 lg:h-8 cursor-pointer bg-slate-100  hover:bg-slate-200 rounded-xl"
               onClick={handleDatabaseCardClick}
             >
               <path fillRule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clipRule="evenodd"
--- components/dashboard/DropDownMenu.tsx
+++ components/dashboard/DropDownMenu.tsx
@@ -24,10 +24,10 @@ export default function DropDownMenu({ uuid, refetchDatabases }: { uuid: string,
         },
     })
     return (
-        <Menu as="div" className="w-8 h-8 inline-block text-left">
+        <Menu as="div" className="w-6 h-6 md:w-8 md:h-8 lg:w-8 lg:h-8 inline-block text-left">
             <div>
-                <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-2 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <Menu.Button className="absolute w-6 h-6 md:w-8 md:h-8 lg:w-8 lg:h-8 inline-flex justify-center gap-x-1.5 rounded-md bg-white px-2 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-[32%] w-2 h-2 md:w-3 md:h-3 lg:w-3 lg:h-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
                     </svg>                </Menu.Button>
             </div>
