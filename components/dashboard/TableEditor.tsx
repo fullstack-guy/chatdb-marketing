048fac074020e45ee315a019936c533124b737ea
fullstack-guy <andriihryhoriev1@gmail.com>
2023-10-05 09:42:00 -0500

    remove console.log

--- components/dashboard/TableEditor.tsx
+++ components/dashboard/TableEditor.tsx
@@ -82,33 +82,32 @@ const TableEditor = ({ tableName, database_uuid }) => {
       const responseData = await response.json();
 
       if (responseData) {
-        console.log(responseData);
         setTableRows(responseData);
         const columns =
           responseData.length > 0
             ? Object.keys(responseData[0]).map((key) => ({
-                key: key,
-                name: key,
-                sortable: true,
-                editable: key === "id" ? false : true,
-                headerRenderer: (props) => (
-                  <div
-                    onClick={() =>
-                      handleSort(key, sortDirection === "asc" ? "desc" : "asc")
-                    }
-                  >
-                    {props.column.name}{" "}
-                    {sortDirection &&
-                      sortColumn === key &&
-                      (sortDirection === "asc" ? (
-                        <FaSortAmountDownAlt />
-                      ) : (
-                        <FaSortAmountUpAlt />
-                      ))}
-                  </div>
-                ),
-                resizable: true,
-              }))
+              key: key,
+              name: key,
+              sortable: true,
+              editable: key === "id" ? false : true,
+              headerRenderer: (props) => (
+                <div
+                  onClick={() =>
+                    handleSort(key, sortDirection === "asc" ? "desc" : "asc")
+                  }
+                >
+                  {props.column.name}{" "}
+                  {sortDirection &&
+                    sortColumn === key &&
+                    (sortDirection === "asc" ? (
+                      <FaSortAmountDownAlt />
+                    ) : (
+                      <FaSortAmountUpAlt />
+                    ))}
+                </div>
+              ),
+              resizable: true,
+            }))
             : [];
         setColumns(columns);
       }
